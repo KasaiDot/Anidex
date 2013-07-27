573bd649aeb057382683501d976a554b98a36e19
Steven Patterson <spatterson8@gmail.com>
2013-07-27 04:55:50 -0400

    Replace button with image

--- gen/com/momentcloud/anidex/R.java
+++ gen/com/momentcloud/anidex/R.java
@@ -21,13 +21,13 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int icon_image_big=0x7f020001;
+        public static final int button_schedule_hdpi_scale=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080002;
         public static final int button_schedule=0x7f080000;
-        public static final int image_splash=0x7f080001;
+        public static final int imagebutton_schedule=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
